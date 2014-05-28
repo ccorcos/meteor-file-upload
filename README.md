434d0d046afd1694ecd76f67a8d1344e2541ec9b
ccorcos <ccorcos@gmail.com>
2014-05-28 15:31:34 -0700

    fixed upload path

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #Simple Meteor File Upload#
 
-This is just a simple meteor app for uploading files.
+This is just a simple meteor app for uploading files. Check out the [demo](http://file-upload.meteor.com).
 
 ##Implementation##
 
--- models/models.js
+++ models/models.js
@@ -1,7 +1,11 @@
-// var base = process.env.PWD
+var base = "";
+
+if (Meteor.isServer) {
+    base = process.env.PWD;
+}
 
 Uploads = new FS.Collection("uploads", {
     stores: [new FS.Store.FileSystem("uploads", {
-        path: "~/code/mrt/simple-cms/uploads"
+        path: base + "/uploads"
     })]
 });
